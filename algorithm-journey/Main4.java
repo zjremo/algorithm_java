a01392f5a8f373f320edd5ef16d98d3366aab9a2
zjremo <3106316195@qq.com>
2026-03-10 01:04:17 +0800

    2026-03-10 复习链表内容
    随机链表复制, 链表partition

--- algorithm-journey/Main4.java
+++ algorithm-journey/Main4.java
@@ -1,4 +1,4 @@
-public class Solution {
+class Solution {
     // Method1: 相交链表
     public static Node getIntersectionNode(Node headA, Node headB) {
         if (headA == null || headB == null) {
@@ -138,4 +138,125 @@ public class Solution {
         return res;
     }
 
+    // Method6: 将单向链表按照某值划分为左边小、中间相等和右边大的形式
+    // 1. 使用数组来做，数组的partition是会的
+    public static Node listPartition(Node head, int pivot){
+        if (head == null)
+            return null;
+        List<Node> list = new ArrayList<>();
+        Node p = head;
+        while (p != null){
+            list.add(p);
+            p = p.next;
+        }
+        // 开始对数组（列表）做partition
+        partition(list, pivot);
+        // 重新串连起来
+        Node prev_head = new Node(), prev = prev_head;
+        for (Node node : list){
+            prev.next = node;
+            prev = prev.next;
+            node.next = null;
+        }
+        return prev_head.next;
+    }
+
+    // 数组的partition操作，参考荷兰国旗问题
+    public static void partition(List<Node> list, int pivot){
+        int less = -1, more = list.size();
+        int l = 0;
+        while (l < more){
+            int val = list.get(l).val;
+            if (val < pivot){
+                Collections.swap(list, ++less, l++);
+            } else if (val > pivot){
+                Collections.swap(list, --more, l);
+            } else {
+                ++l;
+            }
+        }
+    }
+
+
+    // Method6: 将单向链表按照某值划分为左边小、中间相等和右边大的形式
+    // 2. 用有限的变量来做，共使用6个变量
+    public static Node listPartition2(Node head, int pivot){
+        Node sH = null; // small head
+        Node sT = null; // small tail
+        Node eH = null; // equal head
+        Node eT = null; // equal tail
+        Node mH = null; // big head
+        Node mT = null; // big tail
+        // 共分段组织三个链表，然后三个链表连起来
+        Node p = head, next = null;
+        while (p != null){
+            if (p.val < pivot){
+                if (sH == null){
+                    sH = p;
+                    sT = p;
+                } else {
+                    sT.next = p;
+                    sT = p;
+                }
+            } else if (p.val > pivot){
+                if (mH == null){
+                    mH = p;
+                    mT = p;
+                } else {
+                    mT.next = p;
+                    mT = p;
+                }
+            } else {
+                if (eH == null){
+                    eH = p;
+                    eT = p;
+                } else {
+                    eT.next = p;
+                    eT = p;
+                }
+            }
+            next = p.next;
+            p.next = null;
+            p = next;
+        }
+        // 开始串联
+        if (sH !=null){ // 如果有小于区域
+            sT.next = eH;
+            eT = eT == null ? sT : eT; // 确定谁去连接大于区域的头部
+        }
+
+        if (mH != null){ // 小于区域和等于区域不是都没有
+            eT.next = mH;
+        }
+
+        return sH != null ? sH : (eH != null ? eH : mH);
+    }
+
+
+    // Method7: 复制随机链表
+    // 1. 借助哈希表，快速实现复制
+    public static Node copyListWithRand(Node head){
+        if (head == null)
+            return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node p = head;
+        while (p != null){
+            map.put(p, new Node(p.val));
+            p = p.next;
+        }
+        p = head;
+        while (p != null){
+            // 开始复制
+            map.get(p).next = map.get(p.next);
+            map.get(p).random = map.get(p.random);
+            p = p.next;
+        }
+        return map.get(head);
+    }
+
+    // 2. 不借助哈希表
+    // public static Node copyListWithRand2(Node head){
+    //
+    //     return null;
+    // }
 }
